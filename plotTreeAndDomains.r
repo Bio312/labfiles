7a43a844be59a71bafd741b4efeb972105ad9702
joshrest <63354322+joshrest@users.noreply.github.com>
2022-06-12 20:51:05 -0400

    Update plotTreeAndDomains.r

--- plotTreeAndDomains.r
+++ plotTreeAndDomains.r
@@ -15,12 +15,13 @@ if (length(args)<2) {
 
 #install.packages("drawProteins", dependencies = TRUE,repos = "http://cran.us.r-project.org",lib="/home/ec2-user/tools/Rlib4")
 
-install.packages("https://cran.r-project.org/src/contrib/Archive/rlang/rlang_0.4.10.tar.gz", repos = NULL, type="source")
+
 if (!require(devtools)) {
     install.packages('devtools')
 }
 dev_mode(on=TRUE)
 devtools::install_github('brennanpincardiff/drawProteins')
+devtools::install_github("r-lib/rlang")
 
 library(ggtree)
 library(data.table)
