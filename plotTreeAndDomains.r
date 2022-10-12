afe5c6977a27c6d5e11104aa4c89bd9dfa2ac0b1
joshrest <63354322+joshrest@users.noreply.github.com>
2022-10-12 17:45:22 -0400

    Add files via upload

--- plotTreeAndDomains.r
+++ plotTreeAndDomains.r
@@ -2,15 +2,15 @@
 args = commandArgs(trailingOnly=TRUE)
 
 # wget https://raw.githubusercontent.com/Bio312/lab4files/main/plotTreeAndDomains.r
-# sudo /usr/local/bin/Rscript  --vanilla plotTreeAndDomains.r hox.bs.mid.suptree hox.rps-blast.out
+# sudo /usr/local/bin/Rscript  --vanilla plotTreeAndDomains.r hox.bs.mid.suptree hox.rps-blast.out hox.domaintree.pdf
 
 # test if there is at least one argument: if not, return an error
-if (length(args)<2) {
-  stop("You must provide a tree and then domain file.", call.=FALSE)
+if (length(args)<3) {
+  stop("You must provide a tree and then domain file and then output file.", call.=FALSE)
 } 
 
 if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
+install.packages("BiocManager")
 BiocManager::install("drawProteins")
 
 library(ggtree)
@@ -20,12 +20,13 @@ library(ggplot2)
 
 hoxt <- read.tree(args[1]) #"hox.bs.mid.suptree"
 
-t <- ggtree(hoxt, aes(x, y)) + geom_tree() + theme_tree()  + geom_tiplab(cex=0.9) + geom_text2(size=2,aes(subset = !isTip, label=label))
+t <- ggtree(hoxt, aes(x, y)) + geom_tree() + theme_tree()  + geom_tiplab(cex=0.9) + geom_text2(size=2,aes(subset = !isTip, label=label)) + ggplot2::xlim(0, 5.5)
 torder <- data.frame("entryName" = get_taxa_name(t), "order" = length(get_taxa_name(t)):1)
 
 hoxd <- fread(args[2]) #"hox.rps-blast.out"
 
-rel_data0 <- hoxd
+rel_data0 <- hoxd[,c(1:4,6)]
+colnames(rel_data0) <- c("V1","V2","V3","V4","V5")
 rel_data0$type <- "DOMAIN"
 reldatachain0 <- rel_data0[!(duplicated(rel_data0$V1)),]
 reldatachain <- data.frame("V1"="","V2"=reldatachain0$V2,"V3"=1,"V4"=reldatachain0$V2,"V5"=reldatachain0$V1,"type"="CHAIN")
@@ -34,7 +35,7 @@ rel_data1 <- rbind(rel_data0,reldatachain)
 rel_data2 <- as.data.frame(rel_data1[,c(6,5,3,4,2,1,1)])
 rel_data2$taxid <- 1
 colnames(rel_data2) <-  c( "type", "description", "begin","end","length","accession","entryName","taxid")
-rel_data2$description <- sapply(rel_data2$description, function(dx) substr(dx, 1, 20))
+rel_data2$description <- sapply(rel_data2$description, function(dx) substr(dx, 1, 70))
 rel_data3 <- merge(rel_data2,torder)
 
 draw_canvas(rel_data3) -> p
@@ -42,7 +43,7 @@ draw_canvas(rel_data3) -> p
 p <- draw_chains(p, rel_data3,label_chains = FALSE)
 
 
-p <- draw_domains(p, rel_data3,label_domains = FALSE) + theme_bw(base_size = 20) + # white background
+p <- draw_domains(p, rel_data3,label_domains = FALSE,label_size=0.7,show.legend=TRUE) + theme_bw(base_size = 20) + # white background
     theme(panel.grid.minor=element_blank(), 
         panel.grid.major=element_blank()) +
     theme(axis.ticks = element_blank(), 
@@ -55,13 +56,14 @@ p <- draw_domains(p, rel_data3,label_domains = FALSE) + theme_bw(base_size = 20)
         axis.ticks.x=element_blank())+
         theme(legend.title= element_blank())+
      theme(plot.margin=unit(c(-5,5,-5,-5), "mm"))+
- theme(legend.position = c(0.1, 0.15))
+# theme(legend.position = c(0.1, 0.15)) + 
+labs(fill="")
 
-pdf(paste0(args[1],args[2],".pdf"))
+pdf(args[3])
 multiplot(t, p,ncol=2)
 dev.off()
 
-print(paste0(args[1],args[2],".pdf has been outputted"))
+print(paste0(args[3],", a pdf file, has been outputted"))
 
 
 
